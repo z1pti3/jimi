69041b91919b309976eaa5c1d984b35be63381be
Z1P TI3 <z1pti3@outlook.com>
2020-12-16 20:34:55 +0000

    Update

--- react-web/src/components/conductItem.component.js
+++ react-web/src/components/conductItem.component.js
@@ -4,14 +4,18 @@ import PropTypes from "prop-types";
 import "./html.component.css"
 import "./conductItem.component.css"
 
+function truncate(str) {
+    return str.length > 30 ? str.substring(0, 30) + "..." : str;
+}
+
 function ConductItem(props) {
     var d = new Date(0);
     d.setUTCSeconds(props.lastUpdateTime)
     return (
         <div className="conductItemContainer">
             <div className="conductItem">
-                <a className="conductTitle" href={"/conduct/?conductID="+props.id}>
-                    {props.name}
+                <a className="conductTitle" href={"/conduct/?conductID="+props.id} title={props.name.length > 30 ? props.name : ""}>
+                    {truncate(props.name)}
                 </a>
                 <div className="conductRight">
                     <p className="conductLastEdit">
