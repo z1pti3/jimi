935b5f7d6d9ca2e3b35d6a4ed12d26fab920b501
z1pti3 <z1pti3@outlook.com>
2021-01-03 12:46:52 +0000

    Updated storage

--- core/storage.py
+++ core/storage.py
@@ -2,10 +2,12 @@ from core import db
 
 class _storage(db._document):
     fileData = str()
+    source = str()
 
     _dbCollection = db.db["storage"]
 
-    def new(self,acl,fileData):
+    def new(self,acl,source,fileData):
+        self.source = source
         self.fileData = fileData
         self.acl = acl
         return super(_storage, self).new()
