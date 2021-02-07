cc0b3caae0bcc37b19f1354a93009fa51530cd66
z1pti3 <z1pti3@outlook.com>
2021-02-07 12:24:11 +0000

    Added  concurrency threading to forEach

--- system/models/forEach.py
+++ system/models/forEach.py
@@ -3,15 +3,18 @@ import time
 from core.models import action, conduct, webui
 from core import helpers, logging, cache, settings
 
-class _forEach(action._action):
+import jimi
+
+class _forEach(jimi.action._action):
 	manual=bool()
 	eventsField = str()
 	events=list()
 	skip=int()
 	mergeEvents = bool()
 	limit = int()
+	concurrency = int()
 
-	class _properties(webui._properties):
+	class _properties(jimi.webui._properties):
 		def generate(self,classObject):
 			formData = []
 			formData.append({"type" : "input", "schemaitem" : "_id", "textbox" : classObject._id})
@@ -37,7 +40,7 @@ class _forEach(action._action):
 			if self.manual:
 				events = self.events
 			else:
-				events = helpers.evalString(self.eventsField,{"data" : data})
+				events = jimi.helpers.evalString(self.eventsField,{"data" : data})
 			if self.skip == 0:
 				skip = 1
 			else:
@@ -47,6 +50,9 @@ class _forEach(action._action):
 				tempData = conduct.flowDataTemplate(conduct=persistentData["system"]["conduct"],trigger=self,var=data["var"],plugin=data["plugin"])
 				if self.limit > 0:
 					events = events[:self.limit]
+				eventHandler = None
+				if self.concurrency > 0:
+					eventHandler = jimi.workers.workerHandler(self.concurrency,cleanUp=False)
 				for index, event in enumerate(events):
 					if self.limit > 0:
 						if self.limit < index:
@@ -68,9 +74,22 @@ class _forEach(action._action):
 					tempDataCopy["skip"] = skip
 					tempDataCopy["callingTriggerID"] = data["triggerID"]
 
-					persistentData["system"]["conduct"].triggerHandler(data["flowID"],tempDataCopy,flowIDType=True,persistentData=persistentData)
+					if eventHandler:
+						durationRemaining = time.time() - ( persistentData["system"]["trigger"].startTime + persistentData["system"]["trigger"].maxDuration )
+						eventHandler.new("forEachTrigger:{0}".format(data["flowID"]),persistentData["system"]["conduct"].triggerHandler,(data["flowID"],tempDataCopy,False,True,persistentData),maxDuration=durationRemaining)
+					else:
+						persistentData["system"]["conduct"].triggerHandler(data["flowID"],tempDataCopy,flowIDType=True,persistentData=persistentData)
 
 					cpuSaver.tick()
+				# Waiting for all jobs to complete
+				if eventHandler:
+					eventHandler.waitAll()
+					if eventHandler.failureCount() > 0:
+						if jimi.logging.debugEnabled:
+							jimi.logging.debug("Trigger concurrent crash: triggerID={0}".format(self._id),5)
+						jimi.audit._audit().add("forEachTrigger","conccurent crash",{ "forEachID" : self._id, "name" : self.name })
+						raise jimi.exceptions.concurrentCrash
+					eventHandler.stop()
 		# Returning false to stop flow continue
 		actionResult["result"] = False
 		actionResult["rc"] = 200
