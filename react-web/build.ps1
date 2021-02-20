74f2c787e8ff172b0b66dde9dece144380aa86e0
z1pti3 <z1pti3@outlook.com>
2021-02-20 13:31:15 +0000

    Fixed CSS

--- react-web/build.ps1
+++ react-web/build.ps1
@@ -2,6 +2,7 @@ npm run build
 cp ../web/static/css/alert.css build/static/css/alert.css
 cp ../web/static/css/codify.css build/static/css/codify.css
 cp ../web/static/css/conductEditor.css build/static/css/conductEditor.css
+cp ../web/static/css/debugFlowEditor.css build/static/css/debugFlowEditor.css
 cp ../web/static/css/datatable.css build/static/css/datatable.css
 cp ../web/static/css/flow.css build/static/css/flow.css
 cp ../web/static/css/main.css build/static/css/main.css
