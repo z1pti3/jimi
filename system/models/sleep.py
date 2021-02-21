85ecca9ce26761575a294d2e144ec67ac07c3112
z1pti3 <z1pti3@outlook.com>
2021-02-21 21:44:16 +0000

    updated classes for manifest

--- system/models/sleep.py
+++ system/models/sleep.py
@@ -5,16 +5,16 @@ from core.models import action, conduct, webui
 class _sleep(action._action):
 	sleepFor = int()
 
-	class _properties(webui._properties):
-		def generate(self,classObject):
-			formData = []
-			formData.append({"type" : "input", "schemaitem" : "_id", "textbox" : classObject._id})
-			formData.append({"type" : "input", "schemaitem" : "name", "textbox" : classObject.name})
-			formData.append({"type" : "input", "schemaitem" : "sleepFor", "textbox" : classObject.sleepFor, "tooltip" : "Defines in seconds how long to sleep"})
-			formData.append({"type" : "checkbox", "schemaitem" : "enabled", "checked" : classObject.enabled})
-			formData.append({"type" : "checkbox", "schemaitem" : "log", "checked" : classObject.log})
-			formData.append({"type" : "input", "schemaitem" : "comment", "textbox" : classObject.comment})
-			return formData
+	# class _properties(webui._properties):
+	# 	def generate(self,classObject):
+	# 		formData = []
+	# 		formData.append({"type" : "input", "schemaitem" : "_id", "textbox" : classObject._id})
+	# 		formData.append({"type" : "input", "schemaitem" : "name", "textbox" : classObject.name})
+	# 		formData.append({"type" : "input", "schemaitem" : "sleepFor", "textbox" : classObject.sleepFor, "tooltip" : "Defines in seconds how long to sleep"})
+	# 		formData.append({"type" : "checkbox", "schemaitem" : "enabled", "checked" : classObject.enabled})
+	# 		formData.append({"type" : "checkbox", "schemaitem" : "log", "checked" : classObject.log})
+	# 		formData.append({"type" : "input", "schemaitem" : "comment", "textbox" : classObject.comment})
+	# 		return formData
 
 	def run(self,data,persistentData,actionResult):
 		time.sleep(self.sleepFor)
