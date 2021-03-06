e6d8792e8f7bdeef76f29b0fdc12dd306bef21f8
z1pti3 <z1pti3@outlook.com>
2021-03-06 14:52:18 +0000

    Add CLI args

--- jimi_cli.py
+++ jimi_cli.py
@@ -1,2 +1,17 @@
-from screens import mainScreen
-screen = mainScreen.mainScreen()
+import sys
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "reset":
+        if sys.argv[2] == "root":
+            from core import auth
+            from system import install
+            rootUser = auth._user().getAsClass(query={ "username" : "root" })
+            if len(rootUser) == 1:
+                rootUser = rootUser[0]
+                rootPass = install.randomString(30)
+                rootUser.setAttribute("passwordHash",rootPass)
+                rootUser.update(["passwordHash"])
+                print("Password={0}".format(rootPass))
+else:
+    from screens import mainScreen
+    screen = mainScreen.mainScreen()
