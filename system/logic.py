4c1b991bf106982f8bd63b68114c5037c1fb9112
z1pti3 <z1pti3@outlook.com>
2021-07-13 21:10:05 +0000

    #123 - added logic explain to debug

--- system/logic.py
+++ system/logic.py
@@ -5,10 +5,11 @@ from core import logging, helpers, function
 regexLogicString = re.compile(r'((\"(.*?[^\\])\"|([a-zA-Z0-9]+(\[(.*?)\])+)|([a-zA-Z0-9]+(\((.*?)(\)\)|\)))+)|\[(.*?)\]|([a-zA-Z0-9]*)))\s?( not match | match | not in | in |==|!=|>=|>|<=|<)\s?((\"(.*?[^\\])\"|([a-zA-Z0-9]+(\[(.*?)\])+)|([a-zA-Z0-9]+(\((.*?)(\)\)|\))( |$))+)|\[(.*?)\]|([a-zA-Z0-9]*)))')
 regexLogicSafeValidationString = re.compile(r'^(True|False|\(|\)| |or|and|not)*$')
 
-def ifEval(logicString,dicts={}):
+def ifEval(logicString,dicts={},debug=False):
     functionSafeList = function.systemFunctions
     if "if " == logicString[:3]:
         tempLogic = logicString[3:]
+        explainLogin = tempLogic
         logicMatches = regexLogicString.finditer(tempLogic)
         for index, logicMatch in enumerate(logicMatches, start=1):
             statement = [logicMatch.group(1).strip(),logicMatch.group(14).strip(),logicMatch.group(13).strip()]
@@ -16,11 +17,12 @@ def ifEval(logicString,dicts={}):
             for x in range(0,2):
                 statement[x] = helpers.typeCast(statement[x],dicts,functionSafeList)
             tempLogic = tempLogic.replace(logicMatch.group(0),str(logicProcess(statement)))
+            explainLogin = explainLogin.replace(logicMatch.group(0),str(statement))
         # Checking that result only includes True, False, ( ), or, and,
         if regexLogicSafeValidationString.search(tempLogic):
             result = eval(tempLogic) # Can be an unsafe call be very careful with this!
-            if logging.debugEnabled:
-                logging.debug("Action logicEval completed, result='{0}'".format(result),10)
+            if debug:
+                return result, tempLogic, explainLogin
             return result
         else:
             if logging.debugEnabled:
