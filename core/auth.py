38434a3174bce3f797ced4047a81acd61b218f20
z1pti3 <z1pti3@outlook.com>
2021-08-01 16:15:40 +0000

    Removed comment / ref to react

--- core/auth.py
+++ core/auth.py
@@ -408,11 +408,6 @@ if jimi.api.webServer:
             response.headers['Access-Control-Allow-Methods'] = "GET, POST, PUT, DELETE"
             # ClickJacking
             response.headers['X-Frame-Options'] = 'SAMEORIGIN'
-
-            # For NPM WEB DEV
-            # response.headers['Access-Control-Allow-Origin'] = "http://localhost:3000"
-            # response.headers['Access-Control-Allow-Credentials'] = "true"
-            # response.headers['Access-Control-Allow-Methods'] = "GET, POST, PUT, DELETE"
             return response
 
         if jimi.api.webServer.name == "jimi_web":
