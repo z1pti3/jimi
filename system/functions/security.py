adcedd354c229ea0e05b2263f32ee492d35b321d
z1pti3 <z1pti3@outlook.com>
2021-08-09 20:43:35 +0000

    Added min length to encrypt and decrypt string

--- system/functions/security.py
+++ system/functions/security.py
@@ -2,12 +2,16 @@ from core import auth
 
 def encryptString(plaintext,secureString):
     try:
+        if len(secureString) < 4:
+            return ""
         return "ENC {0}".format(auth.getENCFromPassword(plaintext,secureString))
     except:
         return ""
 
 def decryptString(encryptedString,secureString):
     try:
+        if len(secureString) < 4:
+            return ""
         return auth.getPasswordFromENC(encryptedString,secureString)
     except:
         return ""
