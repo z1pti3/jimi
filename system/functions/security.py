8b30bed45c4152c21ee10bb1d92626f01effbe57
z1p <z1pti3@outlook.com>
2021-08-23 20:59:28 +0000

    Added jimi include

--- system/functions/security.py
+++ system/functions/security.py
@@ -1,18 +1,18 @@
-from core import auth
-
 import secrets
+
+import jimi
 
 def encryptString(plaintext,secureString=""):
     try:
         secureString = secureRandom(32)
-        return "ENC {0} {1}".format(auth.getENCFromPassword(plaintext,secureString),secureString)
+        return "ENC {0} {1}".format(jimi.auth.getENCFromPassword(plaintext,secureString),secureString)
     except:
         return ""
 
 def decryptString(encryptedString,secureString=""):
     try:
         secureString = encryptedString.split(" ")[6]
-        return auth.getPasswordFromENC(encryptedString,secureString)
+        return jimi.auth.getPasswordFromENC(encryptedString,secureString)
     except:
         return ""
 
