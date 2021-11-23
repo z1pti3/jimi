33b9bd6f2060304e55374631d3eac48ffbb86522
Patrick Lynch <pynch@protonmail.com>
2021-11-23 00:17:36 +0000

    toaster for successful copy/paste

--- web/build/static/javascript/flow.js
+++ web/build/static/javascript/flow.js
@@ -31,13 +31,13 @@ $(document).ready(function () {
 		if (event.ctrlKey || event.metaKey) {
 			if (event.keyCode == 67 & typeof(document.activeElement["attributes"]["tag"]) == "undefined" & network.getSelectedNodes().length > 0) {
 				$.ajax({url:"/conductEditor/"+GetURLParameter("conductID")+"/copyObjects", data: JSON.stringify({ CSRF: CSRF, "nodes": nodes.get(network.getSelectedNodes()), "edges": edges.get(network.getSelectedEdges()) }), type:"POST", contentType:"application/json", success: function ( result ) {
-					console.log("copied");
+					dropdownAlert(null,"success","Flow Objects Copied!",1000);
 				}
 			});
 			}
 			if (event.keyCode == 86 & typeof(document.activeElement["attributes"]["tag"]) == "undefined" & network.getSelectedNodes().length == 0) {
 				$.ajax({url:"/conductEditor/"+GetURLParameter("conductID")+"/pasteObjects", data: JSON.stringify({ CSRF: CSRF, "centre" : network.getViewPosition() }), type:"POST", contentType:"application/json", success: function ( result ) {
-					console.log("pasted!")
+					dropdownAlert(null,"success","Flow Objects Pasted!",1000);
 				}
 				});
 			}
